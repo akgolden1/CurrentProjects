3cc58930d32f5b1b648ba9645d60a3e548c27645
akgolden1 <akgolden1@users.noreply.github.com>
2021-08-25 07:57:06 -0500

    changes to scriptHelper

--- scriptHelper.js
+++ scriptHelper.js
@@ -16,13 +16,114 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    */
 }
 
+//takes in a string
 function validateInput(testInput) {
+
+    let inputStatus;
+    if(testInput === ""){
+        inputStatus = "Empty";
+    }
+    else if (!isNan(testInput)){
+        inputStatus = "This is a number";
+
+    }
+    else if (isNan(testInput)){
+        inputStatus = "This is not a number";
+    }    
    
+    return inputStatus;
 }
+
+
+
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
    
+ //the user submits a value that is easily converted to the correct data type
+    // The pilot and co-pilot names should be strings 
+    //fuel level and cargo mass should be numbers.
+    let readyStatus;
+    let pilotName = document.querySelector("input[name=pilot]");
+    let coName = document.querySelector("input[name=copilot]");
+    let fuelLevel = document.querySelector("input[name=fuelLevel]");
+    let cargoMass = document.querySelector("input[name=cargoLevel]");
+   
+   
+//     //check for empty fields
+
+if (validateInput (pilotName.value) === "Empty") {
+    alert ("Field is Required");
 }
+else if (validateInput(pilotName.value) === "This is a Number"){
+alert ("This is a Number");
+}
+else {
+    //update per the instructions 
+};
+
+
+
+if (validateInput (copilotName.value) === "Empty") {
+    alert ("Field is Required");
+}
+else if (validateInput(pilotName.value) === "This is a Number"){
+    alert ("This is a Number");
+    }
+    else {
+        //update per the instructions 
+    };
+
+
+
+if (validateInput (fuelLevel.value) === "Empty") {
+    alert ("Field is Required");
+}
+else if (validateInput(fuelLevel.value) === "This is not a Number"){
+    alert ("This is not a Number");
+    }
+    else //check for the level
+       if (fuelLevel.value < 10000){
+
+       let fuelMessage = document.getElementById ("faultyItems");//looking for the faultyItems element
+       
+       fuelMessage.value = fuelMessage.value + "is not enough fuel";//update the falue
+       console.log(fuelMessage.value);//print the current value
+    //or do I need
+    //fuelMessage.innerHTML += "is not enough fuel"
+    //or do I need console.log (fuelMessage.innerHTML);
+       }
+       else {
+           document.getElementByID("faultyItems").style.displays= "block";
+       }
+    
+
+
+
+    if (validateInput (cargoMass.value) === "Empty") {
+        alert ("Field is Required");
+    }
+    else if (validateInput(cargoMass.value) === "This is not a Number"){
+        alert ("This is not a Number");
+        }
+        else {
+            //update per the instructions 
+        };
+    
+    
+    
+};
+
+
+
+//      readyStatus = "Empty";
+
+ 
+
+
+
+     //return readyStatus;
+
+
 
 async function myFetch() {
     let planetsReturned;
