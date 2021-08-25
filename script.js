7b714c945551746de64cd15f5728ebe57d03ccab
akgolden1 <akgolden1@users.noreply.github.com>
2021-08-25 14:04:43 -0500

    I'm tired of committing and getting errors

--- script.js
+++ script.js
@@ -2,16 +2,30 @@
 
 window.addEventListener("load", function() {
 
+
+    //add console.log just to see if we get here
+    console.log("I'm in scripts.js window.addEventListener to see if the page loaded");
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
-   
+//   listedPlanetsResponse.then(function (result) {
+//        listedPlanets = result;
+//        console.log(listedPlanets);
+//    }).then(function () {
+//        console.log(listedPlanets);
+//        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+//    })
+
+   //this is where we put the function to fetch json, so we'd call the function to run everything here;
+
 });
+
+// function someFunction{
+// this is where the purpose of the DOM is to take elemeents on the HTML doc and make them objects
+// that have properties I can work with.
+// //this is where we might build the function referenced in like 19?
+// let someID = document.getElementById("actualIDInTheHTML.")
+// to change the name of something:
+// reference that area of the page with that text:
+// let someIDName = document.getElementById("ActualnameofareaIneedtochangefromhtml"); remember the ids allow us to work with them
+// }
--- scriptHelper.js
+++ scriptHelper.js
@@ -19,22 +19,17 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 //takes in a string
 function validateInput(testInput) {
 
-    let inputStatus;
-    if(testInput === ""){
-        inputStatus = "Empty";
-    }
-    else if (!isNan(testInput)){
-        inputStatus = "This is a number";
-
+    if (testInput === ""){
+        return "Empty";
     }
     else if (isNan(testInput)){
-        inputStatus = "This is not a number";
+        return "This is a not number";
+    }
+    else {
+        return "This is a number";
     }    
    
-    return inputStatus;
 }
-
-
 
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
@@ -42,78 +37,80 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
  //the user submits a value that is easily converted to the correct data type
     // The pilot and co-pilot names should be strings 
     //fuel level and cargo mass should be numbers.
-    let readyStatus;
-    let pilotName = document.querySelector("input[name=pilot]");
-    let coName = document.querySelector("input[name=copilot]");
+    // let readyStatus;
+    let pilotName = document.querySelector("input[name=pilotName]");
+    let copilotName = document.querySelector("input[name=copilotName]");
     let fuelLevel = document.querySelector("input[name=fuelLevel]");
-    let cargoMass = document.querySelector("input[name=cargoLevel]");
+    let cargoMass = document.querySelector("input[name=cargoMass]");
    
    
-//     //check for empty fields
+//     //check for empty fields and prevent the form from loading if there are any
+//try removing the .value after pilotName, copilotName, etc.
 
-if (validateInput (pilotName.value) === "Empty") {
-    alert ("Field is Required");
+if (validateInput (pilotName)||validateInput (copilotName)|| validateInput(fuelLevel) ||
+validateInput (cargoLevel) === "Empty") {
+    alert ("All fields required");
+    event.preventDefault(); //ignore line through event
 }
-else if (validateInput(pilotName.value) === "This is a Number"){
-alert ("This is a Number");
+else if (validateInput(pilotName) === "This is a number"){
+alert ("This is a number");
+console.log ("I'm on formSubmission(validateInput) for pilotname")
+//do something else
 }
-else {
-    //update per the instructions 
-};
-
-
-
-if (validateInput (copilotName.value) === "Empty") {
-    alert ("Field is Required");
+else if (validateInput (copilotName) === "This is a number") {
+    alert ("This is a number");
+    //Do something else
 }
-else if (validateInput(pilotName.value) === "This is a Number"){
-    alert ("This is a Number");
+
+else if (validateInput (fuelLevel) === "This is not a number"){
+    alert ("This is not a number");
+    //do something else
     }
-    else {
-        //update per the instructions 
-    };
-
-
-
-if (validateInput (fuelLevel.value) === "Empty") {
-    alert ("Field is Required");
-}
-else if (validateInput(fuelLevel.value) === "This is not a Number"){
-    alert ("This is not a Number");
-    }
-    else //check for the level
-       if (fuelLevel.value < 10000){
+else if (fuelLevel.value < 10000){
 
        let fuelMessage = document.getElementById ("faultyItems");//looking for the faultyItems element
-       
-       fuelMessage.value = fuelMessage.value + "is not enough fuel";//update the falue
-       console.log(fuelMessage.value);//print the current value
+       fuelMessage.innerHTML += "is not enough fuel"
+       console.log (fuelMessage);//for me
+       //update the fautlyITems??
+
+       document.getElementById("faultyItems").style.visibility = "visible";
+       document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
+       document.getElementById("launchStatus").style.color = "red";//css change?          
+       document.getElementById("fuelStatus").innerHTML = "Fuel level not enough for journey";
+       //fuelMessage.value = fuelMessage.value + "is not enough fuel";//update the falue
+       //console.log(fuelMessage.value);//print the current value
     //or do I need
     //fuelMessage.innerHTML += "is not enough fuel"
-    //or do I need console.log (fuelMessage.innerHTML);
-       }
-       else {
-           document.getElementByID("faultyItems").style.displays= "block";
-       }
-    
-
-
-
-    if (validateInput (cargoMass.value) === "Empty") {
+}
+ else if (validateInput (cargoMass) === "Empty") {
         alert ("Field is Required");
     }
-    else if (validateInput(cargoMass.value) === "This is not a Number"){
-        alert ("This is not a Number");
+    else if (validateInput(cargoMass) === "This is not a number"){
+        alert ("This is not a number");
         }
-        else {
-            //update per the instructions 
-        };
-    
-    
+        else //check for the mass
+        if (cargoMass.value > 10000){
+ 
+        let cargoMass = document.getElementById ("faultyItems");//looking for the faultyItems element
+        
+        cargoMass.value = cargoMass.value + "too much weight";//update the value
+        console.log(cargoMass);//print the current value
+
+        document.getElementById("faultyItems").style.visibility = "visible";
+        document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready for Launch";
+        document.getElementById("launchStatus").style.color = "red";//do I need to do something with css?
+        document.getElementById("fuelStatus").innerHTML = "Too much mass for takeoff";
+     //or do I need
+     //cargoMass.innerHTML += "is not enough fuel"
+     //or do I need console.log (cargoMass.innerHTML);
+       // }
+        //else {
+            //document.getElementByID("faultyItems").style.displays= "block";//show the div
+   //     }
     
 };
 
-
+ 
 
 //      readyStatus = "Empty";
 
@@ -121,15 +118,12 @@ else if (validateInput(fuelLevel.value) === "This is not a Number"){
 
 
 
-     //return readyStatus;
-
-
 
 async function myFetch() {
-    let planetsReturned;
+    // let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    // planetsReturned = await fetch().then( function(response) {
+    //     });
 
     return planetsReturned;
 }
