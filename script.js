0d2e0f5142494c60104c42bf16411174e6d137f2
akgolden1 <akgolden1@users.noreply.github.com>
2021-08-26 15:16:01 -0500

    re-did everything from the original code. Updated scriptHelper

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 // Write your JavaScript code here!
+
 window.addEventListener("load", function() {
+   let form = document.querySelector("form");//lines 4-6 explained in 25.9. We need the variable form because it's getting a reference.
+   form.addEventListener("submit", function (event){
+let pilotNameInput = document.querySelector("input[name=pilotName]"); //we need the usernameInput because we're returing a reference
+let copilotNameInput = document.querySelector("input[name=copilotName]");
+let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+let cargoMassInput = document.querySelector("input[name=cargoMass]"); 
+   //alert ("username: " + pilotNameInput.value)// still work to do, this is the syntax
+       // event.defaultPrevented();//I think I want this here. From 25.9
+    //validateInput ();
+
+if (pilotNameInput.value === "" || copilotNameInput.value ==="" ||fuelLevelInput.value ===""||cargoMassInput.value ===""){
+ alert("No empty fields");
+}
+  });
+});
+
+   //validateInput (inputStatus);
+   //somehow I have to tie this to the scriptHelper so that this loads first
+
+
+
 
    let listedPlanets;
-   let listedPlanetsResponse = myFetch();
+   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+   let listedPlanetsResponse;
    listedPlanetsResponse.then(function (result) {
-      listedPlanets = result;
-      console.log(listedPlanets);
-         }).then(function () {
-         console.log(listedPlanets);
-      });
-
-      let pilotName = document.querySelector("input[name=pilotName]");
-      console.log (pilotName);
-      
-});
+       listedPlanets = result;
+       console.log(listedPlanets);
+   }).then(function () {
+       console.log(listedPlanets);
+       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+   })
+   
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,147 +1,112 @@
+
+//One bite at a time!!
+//Step 1, we know we're creating a checklist form
+//We have 4 fields that need to be filled out with the pilot (Chris) and copilot name (Blake), fuel levels and cargo mass
+//We're using validation to ensur that we have all the info and make sure no one prematurel launches the shuttle
 // Write your helper functions here!
+
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
-//<div id="missionTarget" data-testid="missionTarget">
-let divID = document.getElementById("missionTarget");
-divID.innerHTML = `
+   /*
                 <h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: ${json[missionTarget].name} </li>
-                    <li>Diameter: ${json[missionTarget].diameter}  </li>
-                    <li>Star: ${json[missionTarget].start} }</li>
-                    <li>Distance from Earth: ${json[missionTarget].distance}  </li>
-                    <li>Number of Moons: ${json[missionTarget].moons}  </li>
+                    <li>Name: </li>
+                    <li>Diameter: </li>
+                    <li>Star: ${star}</li>
+                    <li>Distance from Earth: </li>
+                    <li>Number of Moons: </li>
                 </ol>
-                <img src="${json[missionTarget].image}"></img>;
-`} //I only added the ticks because of 26.3. I don't really understand why that works
+                <img src="">
+   */
+}
 
-//takes in a string
+  let pilotNameInput = document.querySelector("input[name=pilotName]"); //we need the usernameInput because we're returing a reference
+  let copilotNameInput = document.querySelector("input[name=copilotName]");
+  let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+  let cargoMassInput = document.querySelector("input[name=cargoMass]");
+  
+
+  //I also need these so that I can update them:
+  let showlLaunchStatus = document.getElementById('launchStatus');
+  let showFaultyItems = document.getElementById('faultyItems');
+
+  let showPilotStatus = document.getElementById('pilotStatus');
+  let showCopilotStatus = document.getElementById('copilotStatus');
+  let showFuelStatus = document.getElementById('fuelStatus');
+  let showCargoStatus = document.getAnimations ('cargoStatus');
+
+
+
 function validateInput(testInput) {
-
-    if (testInput === ""){
-        return "Empty";
+   //form info from index.html file: name ="pilotName",name= "copilotName",name="fuelLevel", name="cargoMass"
+ //this is taking a string. That string testInput is the input the user is putting into the form
+ //use console.log until this is connected to script.js
+ if (pilotNameInput.value === "" || copilotNameInput.value ==="" ||fuelLevelInput.value ===""||cargoMassInput.value ===""){
+    return "Empty";
     }
-    else if (isNan(testInput)){
-        return "This is a not number";
+
+if (!isNaN(pilotNameInput.value) ||!isNaN(copilotNameInput.value) ){
+    return "Is a number";//we don't want it to be a number
     }
-    else {
-        return "This is a number";
-    }    
-   
-}
-
-
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
- //the user submits a value that is easily converted to the correct data type
-    // The pilot and co-pilot names should be strings 
-    //fuel level and cargo mass should be numbers.
-    // let readyStatus;
-    let pilotName = document.querySelector("input[name=pilotName]");
-    let copilotName = document.querySelector("input[name=copilotName]");
-    let fuelLevelAmt = document.querySelector("input[name=fuelLevel]");
-    let cargoMass = document.querySelector("input[name=cargoMass]");
-   
-   
-//     //check for empty fields and prevent the form from loading if there are any
-//try removing the .value after pilotName, copilotName, etc.
-
-if (validateInput (pilotName)||validateInput (copilotName)|| validateInput(fuelLevelAmt) ||
-validateInput (cargoLevel) === "Empty") {
-    alert ("All fields required");
-   
-}
-else if (validateInput(pilotName) === "This is a number"){
-alert ("This is a number");
-console.log ("I'm on formSubmission(validateInput) for pilotname")
-//do something else
-}
-else if (validateInput (copilotName) === "This is a number") {
-    alert ("This is a number");
-    //Do something else
-}
-
-else if (validateInput (fuelLevelAmt) === "This is not a number"){
-    alert ("This is not a number");
-    //do something else
+else if (isNaN(fuelLevelInput.value)||isNaN(cargoMassInput.value)){
+    return ("Not a Number");
     }
-else if (fuelLevelAmt.value < 10000){
-
-       let fuelMessage = document.getElementById ("faultyItems");//looking for the faultyItems element
-       fuelMessage.innerHTML += "is not enough fuel"
-       console.log (fuelMessage);//for me
-       //update the fautlyITems??
-
-       document.getElementById("faultyItems").style.visibility = "visible";
-       document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch";
-       document.getElementById("launchStatus").style.color = "red";//css change?          
-       document.getElementById("fuelStatus").innerHTML = "Fuel level not enough for journey";
-       //fuelMessage.value = fuelMessage.value + "is not enough fuel";//update the falue
-       //console.log(fuelMessage.value);//print the current value
-    //or do I need
-    //fuelMessage.innerHTML += "is not enough fuel"
-}
- else if (validateInput (cargoMass) === "Empty") {
-        alert ("Field is Required");
-    }
-    else if (validateInput(cargoMass) === "This is not a number"){
-        alert ("This is not a number");
-        }
-        else //check for the mass
-        if (cargoMass.value > 10000){
- 
-        let cargoMass = document.getElementById ("faultyItems");//looking for the faultyItems element
-        
-        cargoMass.value = cargoMass.value + "too much weight";//update the value
-        console.log(cargoMass);//print the current value
-
-        document.getElementById("faultyItems").style.visibility = "visible";
-        document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready for Launch";
-        document.getElementById("launchStatus").style.color = "red";//do I need to do something with css?
-        document.getElementById("fuelStatus").innerHTML = "Too much mass for takeoff";
-     //or do I need
-     //cargoMass.innerHTML += "is not enough fuel"
-     //or do I need console.log (cargoMass.innerHTML);
-       // }
-        //else {
-            //document.getElementByID("faultyItems").style.displays= "block";//show the div
-   //     }
- }
-
-    else if (fuelLevelAmt.value >= 10000 && cargoMass.value <= 10000  ){
-
-    document.getElementById("faultyItems").style.visibility = "visible";
-    document.getElementById("launchStatus").innerHTML = "Shuttle is ready for launch";
-    document.getElementById("launchStatus").style.color = "green";//css change?  
-    //also something like: 
-   console.log (pilotName + "Is ready for launch");
-
-    }
-}
-
-
-async function myFetch() {//I guess we're waiting for myFetch to finish?
-    let planetsReturned=0;//returns the promise?
-    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
-console.log(response.json);
-    return (response.json());
-});
-
-console.log (planetsReturned);
-    return planetsReturned;//existing code
 };
+
+//update the table on the bottom of the page based on the return values
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {//this function updates the values
+   if (validateInput(pilotNameInput.value)==="Is a number" || validateInput(copilotNameInput.value)==="Is a number"){
+            alert ("Make sure to enter valid information for each field!");
+        }
+        else { //update final table
+            showPilotStatus.innerHTML = `Pilot ${pilotName.value} is ready`;
+            showCopilotStatus.innerHTML = `Co-pilot ${copilotName.value} is ready`;
+        }//this may not be enough
+};
+    if (validateInput(fuelLevelInput.value)==="Not a number" || validateInput(cargoMassInput.value)==="Not a number")
+        {
+            alert ("Make sure to enter valid information for each field!");
+
+        }else if ((validateInput(cargoMassInput.value) > 10000)){  // Is the cargo mass above 10,000?
+            showFaultyItems.style.visibility = 'visible';
+            showCargoStatus.innerHTML = `Cargo mass too heavy for the shuttle to take off!`; 
+            showLaunchStatus.innerHTML = `Shuttle not ready for launch`;
+            showLaunchStatus.style.color = 'red';
+
+         } else if ((validateInput(fuelLevelInput.value)) < 10000) {
+
+            showFaultyItems.style.visibility = 'visible';
+            showFuelStatus.innerHTML = `Shuttle ready for launch`;
+            showLaunchStatus.innerHTML = `fuel too low for launch`;
+            showLaunchStatus.style.color = 'red';
+         }
+
+
+         if((fuelLevelInput.value >= 10000) && (cargoMassInput.value <= 10000)) { // If the fuelLevel is equal to or above 10,000 AND cargoMass is equal to or below 10,000, execute this code 
+            showFaultyItems.style.visibility = 'visible';
+            showLaunchStatus.innerHTML = `Shuttle is ready for launch`;
+            showLaunchStatus.style.color = 'green';
+            showFuelStatus.innerHTML = `Fuel level high enough for launch`;
+            showCargoStatus.innerHTML = `Cargo mass low enough for launch`;
+         }
+
+
+
+
+
+async function myFetch() {
+    let planetsReturned;
+
+    planetsReturned = await fetch().then( function(response) {
+        });
+
+    return planetsReturned;
+}
 
 function pickPlanet(planets) {
-//takes in one argument, a list of planets and returns one planet from the list with a 
-//randomly-selected index
-let returnedIndex=0;
-
-returnedIndex = planets[Math.random()*6];//take my array[Math.random()*number of objects in my array]
-return returnedIndex;
-
-};
+}
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
