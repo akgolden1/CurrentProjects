ffc2707f1223a1598c2a53fae604a2aab4fe7ff0
akgolden1 <akgolden1@users.noreply.github.com>
2021-08-29 16:09:04 -0500

    cleaning up ccode to see if that helps

--- script.js
+++ script.js
@@ -3,25 +3,16 @@
 
 window.addEventListener("load", function() {
   
-   //let myForm = document.getElementById("testForm");
-let form = document.querySelector("form");
-    form.addEventListener("submit", function(event){
-        alert("submit clicked");
+let myForm = document.getElementById("launchForm");
+
+    myForm.addEventListener("submit", function(event){
+        //alert("submit clicked");
         
     });
 
     //let pilotName = document.getElementById('pilotName.value');
     let pilotName=document.querySelector("input[name=pilotName]");
-    let copilotName = document.getElementById('copilotName');
-    let fuelLevel = document.getElementById('fuelLevel');
-    let cargoMass = document.getElementById('cargoMass');
-    let launchStatus = document.getElementById('launchStatus');
-    let listFaultyItems = document.getElementById('faultyItems');//just to be difficult
-    
-    let pilotStatus = document.getElementById('pilotStatus');
-    let copilotStatus = document.getElementById('copilotStatus');
-    let fuelStatus = document.getElementById('fuelStatus');
-    let cargoStatus = document.getElementById ('cargoStatus');
+ 
 
 
     let listedPlanets;
@@ -34,7 +25,8 @@ let form = document.querySelector("form");
         console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
 
- formSubmission(document, listFaultyItems, pilotName.value, copilotName, fuelLevel, cargoMass);//calling it here is working
+
+ formSubmission(document, listFaultyItems, pilotName, copilotName, fuelLevelAmt, cargoMass);//calling it here is working
     });
  });
 
--- scriptHelper.js
+++ scriptHelper.js
@@ -32,7 +32,23 @@ else {
 
 
 
+
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+
+
+        //let pilotName = document.getElementById('pilotName.value');
+        // let pilotName=document.querySelector("input[name=pilotName]");
+        // let copilotName = document.querySelector("input[name=copilotName]");
+        // let fuelLevelAmt = document.querySelector("input[name=fuelLevel]");
+        // let cargoMass = document.querySelector("input[name=cargoMass]");
+        // let launchStatus = document.getElementById('launchStatus');
+        // let listFaultyItems = document.getElementById('faultyItems');//just to be difficult
+        
+        // let pilotStatus = document.getElementById('pilotStatus');
+        // let copilotStatus = document.getElementById('copilotStatus');
+        // let fuelStatus = document.getElementById('fuelStatus');
+        // let cargoStatus = document.getElementById ('cargoStatus');
+    
   console.log (pilotName.value);
    alert ("I'm in form submission");
    alert (pilotName);
@@ -47,7 +63,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
            copilotStatus.innerHTML = `Co-pilot ${copilotName.value} is ready`;
        }//this may not be enough
    }
-   if (validateInput(fuelLevel.value)==="Not a number" || validateInput(cargoMass.value)==="Not a number")
+   if (validateInput(fuelLevelAmt.value)==="Not a number" || validateInput(cargoMass.value)==="Not a number")
        {
            alert ("Make sure to enter valid information for each field!");
 
@@ -57,7 +73,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
            launchStatus.innerHTML = `Shuttle not ready for launch`;
            launchStatus.style.color = 'red';
 
-        } else if ((validateInput(fuelLevel.value)) < 10000) {
+        } else if ((validateInput(fuelLevelAmt.value)) < 10000) {
 
            faultyItems.style.visibility = 'visible';
            fuelStatus.innerHTML = `Shuttle ready for launch`;
@@ -65,7 +81,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
            launchStatus.style.color = 'red';
         }
 
-        if((fuelLevel.value >= 10000) && (cargoMass.value <= 10000)) { // If the fuelLevel is equal to or above 10,000 AND cargoMass is equal to or below 10,000, execute this code 
+        if((fuelLevelAmt.value >= 10000) && (cargoMass.value <= 10000)) { // If the fuelLevel is equal to or above 10,000 AND cargoMass is equal to or below 10,000, execute this code 
            faultyItems.style.visibility = 'visible';
            launchStatus.innerHTML = `Shuttle is ready for launch`;
            launchStatus.style.color = 'green';
@@ -75,16 +91,30 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
    
 
-async function myFetch() {//I guess we're waiting for myFetch to finish?
-    //returns the promise?
-    let planetsReturned = 0;
-    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
-     console.log(response.json);
-        // return response.json();//the json data is attached to myFetch. This will some back later.
-     });
+        async function myFetch() {
+            let planetsReturned;
+        
+            planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+                });
+        console.log (planetsReturned);
+            return planetsReturned;
+        }
+        
 
-      return planetsReturned;
-}
+// async function myFetch() {//I guess we're waiting for myFetch to finish?
+//     //returns the promise?
+//     let planetsReturned = 0;
+//     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+//      console.log(response.json);
+//         // return response.json();//the json data is attached to myFetch. This will some back later.
+//      });
+
+//       return planetsReturned;
+// }
+
+
+
+
 
 //randomizer just picks a random planet. The planets are what I'm fetching
 function pickPlanet(planets) {//this just produces a number, that I will use later to pick a planet
